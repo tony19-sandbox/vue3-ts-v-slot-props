fad640ded194189878644a8dbac271b5c6a869a5
Tony Trinh <tony19@gmail.com>
2022-04-12 16:23:33 -0500

    try to repro issue

---
+++ src/components/DataWrapper.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const data = {
+  title: 'Data Wrapper',
+  description: 'A wrapper for data',
+  data: {
+    name: 'John Doe',
+    age: 30,
+    address: {
+      street: '123 Main St',
+      city: 'Anytown',
+      state: 'CA'
+    }
+  }
+}
+</script>
+
+<template>
+  <slot :values="data"></slot>
+</template>
